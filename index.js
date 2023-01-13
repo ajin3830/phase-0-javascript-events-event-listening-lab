9ffaa47efb945ac5e17ca22f8b880dc645c8645e
ajin3830 <ajin3830@gmail.com>
2023-01-13 09:56:08 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// create an alert
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', () => alert('I was clicked!'));
 }
+addingEventListener()
+
+// log something
+document.querySelector("#button").addEventListener('mouseover', () => console.log('that tickles!'))
+
+// create a different alert
+document.querySelector("#button").addEventListener('click', () => alert('Hey I was clicked!'))
+
+
